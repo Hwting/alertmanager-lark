e38cc8de2e0e4e7046268ce2ea98230526888e05
unknown <837479851@qq.com>
2020-01-02 18:39:48 +0800

    优化

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ app.debug = True
 def add_stu():
     if  not request.data:   #检测是否有数据
         return ('fail')
-    student = request.data.decode('utf-8')
+    r = request.data.decode('utf-8')
     #获取到POST过来的数据，因为我这里传过来的数据需要转换一下编码。根据晶具体情况而定
-    sed_json = json.loads(student)
+    sed_json = json.loads(r)
     #把区获取到的数据转为JSON格式。
     return jsonify(sed_json)
     #返回JSON数据。
